1eccd84b7d5615693c60da600b5e491731756262
Baranov-Dmitry <Baranov-Dmitry@users.noreply.github.com>
2020-07-26 23:01:50 +0300

    Done angular basics

---
+++ src/app/post4/post4.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-post4',
+    template: `
+      <div class="post4">
+        <h2>Post4 title</h2>
+      </div>
+    `,
+    styles: [`
+      .post4 {
+        color: green;
+      }
+    `]
+})
+export class Post4Component {
+
+}
