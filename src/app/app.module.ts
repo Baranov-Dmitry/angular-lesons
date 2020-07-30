34bda3f3007e74151e915a309a5532989c553fe2
Baranov-Dmitry <Baranov-Dmitry@users.noreply.github.com>
2020-07-30 14:30:53 +0300

    status: done derectives task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms'
+import { StyleDirective } from './directives/style.directive';
+import { IfnotDirective } from './directives/ifnot.derective';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleDirective,
+    IfnotDirective
   ],
   imports: [
     BrowserModule,
