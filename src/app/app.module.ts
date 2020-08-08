b8e2d80798f148caacc255ff56d8f8f47f952796
Baranov-Dmitry <Baranov-Dmitry@users.noreply.github.com>
2020-08-08 12:38:53 +0300

    status: creating base structure of blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { SwitchComponent } from './switch/switch.component'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SwitchComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
-  providers: [
-
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
