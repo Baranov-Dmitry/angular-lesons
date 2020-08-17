e3d2b1504f3ec015ebf0e5f7438290bbaaa5b3e6
Baranov-Dmitry <Baranov-Dmitry@users.noreply.github.com>
2020-08-17 19:43:16 +0300

    progres: adding farebase property, create routing logic and include nessasary servises and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  //rout for main component and their children
+  {path: '', component: MainLayoutComponent, children: [
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+    {path: '', component: HomePageComponent},
+    //component for show posts
+    {path: 'post/:id', component: PostPageComponent},
+  ]},
+  //rout for admin component and their children
+  {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  //forRoot that means this is main router
+  imports: [RouterModule.forRoot(routes, {
+    //after page has been loaded starting download other modules
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyC7rrVRz0cfcZIb4Lsq46htixb3DFfXVI8'
 };
